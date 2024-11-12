e54938aaeea84cf4fc388040f0d89c44191455b4
何泽颖 <hezeying@autowise.ai>
2024-11-12 15:08:37 +0800

    feat:文案修改

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,11 +1,21 @@
+/*
+ * @Author: 何泽颖 hezeying@autowise.ai
+ * @Date: 2024-03-08 16:21:39
+ * @LastEditors: 何泽颖 hezeying@autowise.ai
+ * @LastEditTime: 2024-11-12 15:08:29
+ * @FilePath: /NotionNext/components/ThemeSwitch.js
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
 import { useGlobal } from '@/lib/global'
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Draggable } from './Draggable'
-import { THEMES } from '@/themes/theme'
-import { useRouter } from 'next/router'
+// import { THEMES } from '@/themes/theme'
+// import { useRouter } from 'next/router'
 import DarkModeButton from './DarkModeButton'
-import { getQueryParam } from '@/lib/utils'
-import LANGS from '@/lib/lang'
+// import { getQueryParam } from '@/lib/utils'
+// import LANGS from '@/lib/lang'
 /**
  *
  * @returns 主题切换
